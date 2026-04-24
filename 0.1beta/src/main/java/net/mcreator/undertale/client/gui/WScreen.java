167d98d71e4b972b11d1e0fd0a2939aff6785541
MoCurieZzx <mojulizhangzhixiang@gmail.com>
2026-04-24 21:34:15 +0800

    Add files via upload

---
+++ 0.1beta/src/main/java/net/mcreator/undertale/client/gui/WScreen.java
@@ -0,0 +1,75 @@
+package net.mcreator.undertale.client.gui;
+
+import net.minecraft.world.level.Level;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.network.chat.Component;
+import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
+import net.minecraft.client.gui.GuiGraphics;
+
+import net.mcreator.undertale.world.inventory.WMenu;
+import net.mcreator.undertale.init.UndertaleModScreens;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+
+public class WScreen extends AbstractContainerScreen<WMenu> implements UndertaleModScreens.ScreenAccessor {
+	private final Level world;
+	private final int x, y, z;
+	private final Player entity;
+	private boolean menuStateUpdateActive = false;
+	private static final ResourceLocation BACKGROUND = ResourceLocation.parse("undertale:textures/screens/w.png");
+	private static final ResourceLocation IMAGE_0 = ResourceLocation.parse("undertale:textures/screens/waterfall_damp_wood_hanging_sign.png");
+
+	public WScreen(WMenu container, Inventory inventory, Component text) {
+		super(container, inventory, text);
+		this.world = container.world;
+		this.x = container.x;
+		this.y = container.y;
+		this.z = container.z;
+		this.entity = container.entity;
+		this.imageWidth = 176;
+		this.imageHeight = 59;
+	}
+
+	@Override
+	public void updateMenuState(int elementType, String name, Object elementState) {
+		menuStateUpdateActive = true;
+		menuStateUpdateActive = false;
+	}
+
+	@Override
+	public void render(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTicks) {
+		super.render(guiGraphics, mouseX, mouseY, partialTicks);
+		this.renderTooltip(guiGraphics, mouseX, mouseY);
+	}
+
+	@Override
+	protected void renderBg(GuiGraphics guiGraphics, float partialTicks, int mouseX, int mouseY) {
+		RenderSystem.setShaderColor(1, 1, 1, 1);
+		RenderSystem.enableBlend();
+		RenderSystem.defaultBlendFunc();
+		guiGraphics.blit(BACKGROUND, this.leftPos, this.topPos, 0, 0, this.imageWidth, this.imageHeight, this.imageWidth, this.imageHeight);
+		guiGraphics.blit(IMAGE_0, this.leftPos + 14, this.topPos + 21, 0, 0, 16, 16, 16, 16);
+		RenderSystem.disableBlend();
+	}
+
+	@Override
+	public boolean keyPressed(int key, int b, int c) {
+		if (key == 256) {
+			this.minecraft.player.closeContainer();
+			return true;
+		}
+		return super.keyPressed(key, b, c);
+	}
+
+	@Override
+	protected void renderLabels(GuiGraphics guiGraphics, int mouseX, int mouseY) {
+		guiGraphics.drawString(this.font, Component.translatable("gui.undertale.w.label_empty"), 38, 23, -16183142, false);
+	}
+
+	@Override
+	public void init() {
+		super.init();
+	}
+}
